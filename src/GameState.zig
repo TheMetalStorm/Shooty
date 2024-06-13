4f9ec79011ede851204c0fa17a477d8589df5ec0
TheMetalStorm <arapoglu.simon@gmx.de>
2024-06-13 23:34:03 +0200

    fixed speed powerup bug

--- src/GameState.zig
+++ src/GameState.zig
@@ -35,7 +35,6 @@ var gpa: std.mem.Allocator = undefined;
 pub const DEBUG = false;
 const bgSpriteRect: rl.Rectangle = rl.Rectangle.init(0.0, 0.0, 128, 256);
 
-//TODO: BUG: sometimes the player gets damaged when he is in speed form (i think it has to do with picking up an item while in speed form)
 //TODO: refactor collision out of enemy class into Game State
 //TODO: add sound effects
 //TODO: ship it (web)
